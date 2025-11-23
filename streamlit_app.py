e1dc5dd12a351465708d0e187870f5d63c195dde
kavisandihasithma-dotcom <kavisandihasithma@gmail.com>
2025-11-23 20:01:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ col1,col2 = st.columns(2)
 with col1:
     st.header("This is column 1")
     st.write("This is the left column")
+    st.image("https://mbluxury1.s3.amazonaws.com/2022/02/25172711/versace.jpg")
 
 with col2:
     st.header("This is column 2")
+    st.button("Clike me")
