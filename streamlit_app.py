75b62cadb32e66e8f4952e906bdcb1a08d4742fa
kavisandihasithma-dotcom <kavisandihasithma@gmail.com>
2025-11-23 19:59:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ st.title("My Streamlit App Layout Demo")
 
 col1,col2 = st.columns(2)
 with col1:
-    st.head("This is column 1")
+    st.header("This is column 1")
     st.write("This is the left column")
 
 with col2:
-    st.head("This is column 2")
+    st.header("This is column 2")
