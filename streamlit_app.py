b9ee975dfce1fd01b29a8a71a37bdc3dfae7bcf6
kavisandihasithma-dotcom <kavisandihasithma@gmail.com>
2025-11-23 18:13:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My Hasithma app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.markdown("# My App Title")
+st.markdown("This is **bold** text.")
+st.markdown("- Item 1\n- Item 2")
+
