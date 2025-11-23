cb69b5f8e87e1129609c2326c8007418421c3df1
kavisandihasithma-dotcom <kavisandihasithma@gmail.com>
2025-11-23 18:42:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("My First Streamlit App")
 st.markdown("Welcome to this introductory Streamlit app!")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2, 1])
 
-col1.write("This is Column 1")
-col2.write("This is Column 2")
+col1.write("Wide column")
+col2.write("Narrow column")
