ec746bd3a7abc1559d9ca630fa6881a5b543754a
kavisandihasithma-dotcom <kavisandihasithma@gmail.com>
2025-11-26 07:57:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,23 @@
 import streamlit as st
-
-st.title("My Streamlit App Layout Demo")
-
-col1,col2 = st.columns(2)
-with col1:
-    st.header("This is column 1")
-    st.write("This is the left column")
-    st.image("https://mbluxury1.s3.amazonaws.com/2022/02/25172711/versace.jpg")
-
-with col2:
-    st.header("This is column 2")
-    st.button("Clike me")
+# Create two rows and three columns
+row1_col1, row1_col2, row1_col3 = st.columns(3)
+row2_col1, row2_col2, row2_col3 = st.columns(3)
+# Add content to the cells
+with row1_col1:
+ st.write("Row 1, Column 1")
+ # Add your content for this cell
+with row1_col2:
+ st.write("Row 1, Column 2")
+ # Add your content for this cell
+ ith row1_col3:
+ st.write("Row 1, Column 3")
+ # Add your content for this cell
+with row2_col1:
+ st.write("Row 2, Column 1")
+ # Add your content for this cell
+with row2_col2:
+ st.write("Row 2, Column 2")
+ # Add your content for this cell
+with row2_col3:
+ st.write("Row 2, Column 3")
+ # Add your content for this cell
